d1ab417e48cb9e5b4bb82e0c919bcae5098f260a
Carl Markham <doddsey65@gmail.com>
2018-01-02 13:19:21 +0000

    update TLD from dev to devel

--- .zsh/functions.sh
+++ .zsh/functions.sh
@@ -1,50 +1,10 @@
 #!/usr/local/bin/zsh
 
-
-
-db-dump() {
-  DB_DIR="/Users/$USER/iCloud Drive/Database Backups"
-  DB_NAME=$1
-  DB_FILE="$DB_DIR/$DB_NAME-$(date +%y%m%d).dump"
-  if [ -e $DB_FILE ]; then
-    echo "\033[31mBackup file ${DB_FILE} already exists"
-    return 127
-  fi
-  COMMAND="pg_dump -Fc --no-acl --no-owner -h localhost -U $USER $DB_NAME > \"$DB_FILE\""
-  echo $COMMAND
-  zsh -c "$COMMAND"
-  cp $DB_FILE $DB_DIR/$DB_NAME-latest.dump
-}
-
-db-restore() {
-  DB_DIR="/Users/$USER/iCloud Drive/Database Backups"
-  DB_NAME=$1
-  DB_FILE="$DB_DIR/$DB_NAME-latest.dump"
-  if [ ! -f "$DB_FILE" ]; then
-    echo "\033[31mBackup file $DB_FILE does not exist"
-    return 127
-  fi
-  COMMAND="pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $USER -d $DB_NAME \"$DB_FILE\""
-  echo $COMMAND
-  zsh -c "psql -U $USER -c \"CREATE DATABASE $DB_NAME;\""
-  zsh -c "$COMMAND"
-}
-
-
-
-docker-init () {
-  eval "$(docker-machine env fgdo)"
-}
-
-
-
 flushdns () {
   dscacheutil -flushcache
   sudo killall -HUP mDNSResponder
   sudo brew services restart dnsmasq
 }
-
-
 
 update-dotfiles() {
   cd ~/src/givey/dotfiles
@@ -54,26 +14,25 @@ update-dotfiles() {
   cd -
 }
 
-
-
-src() {
-  cd ~/src/$1
+web() {
+  cd ~/src/givey/web
 }
-
-
 
 rspec() {
   bundle exec rspec $@
 }
 
-
-
 lock-screen() {
   automator ~/.dotfiles/automator/Lock\ Screen.workflow
 }
 
-
-
-boost-timemachine() {
-  sudo sysctl debug.lowpri_throttle_enabled=0
+remove-all() {
+  brew remove --force --ignore-dependencies $(brew list)
+  rm -rf ~/.Brewfile
+  rm -rf ~/.gitconfig
+  rm -rf ~/.gitconfig-user
+  rm -rf ~/.gitignore-global
+  rm -rf ~/.zsh
+  rm -rf ~/.zshrc
+  rm -rf ~/iCloud\ Drive
 }
--- .zshrc
+++ .zshrc
@@ -15,9 +15,8 @@ eval "$(nodenv init -)"
 # Android Development
 export ANT_HOME=/usr/local/opt/ant
 export MAVEN_HOME=/usr/local/opt/maven
-export GRADLE_HOME=/usr/local/opt/gradle
-export ANDROID_HOME=/usr/local/opt/android-sdk
-export ANDROID_NDK_HOME=/usr/local/opt/android-ndk
+export GRADLE_HOME=/usr/local/bin/gradle
+export ANDROID_HOME=/usr/local/Caskroom/android-sdk/25.2.3
 export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 
